9ef5c3c60d9828ba8436c6310bae3da9344929c6
Prashanna Sthapit <139867239+prashannasthapit@users.noreply.github.com>
2023-11-30 22:16:15 +0545

    Increased functionality to include negative numbers Testing improved, generating random ints

--- src/main/java/org/august/Main.java
+++ src/main/java/org/august/Main.java
@@ -1,39 +1,34 @@
 package org.august;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        List<Integer> numbers = new ArrayList<>();
+        int count = 1000; // number of random integers generated
+        int minInclusive = -2147483648; // minimum (inclusive) value
+        int maxExclusive = 2147483647; // maximum (exclusive) value
 
-        for (int i = 0; i < 1000; i++) {
-            numbers.add(randInt(0, 999999));
-        }
+        SecureRandom secureRandom = new SecureRandom();
+        List<Integer> numbers = secureRandom.ints(count, minInclusive, maxExclusive).boxed().toList();
 
-        int total=0;
-        int totalPar=0;
+        int total = 0;
+        int totalPar = 0;
 
         for (int number : numbers) {
             long startTime = System.nanoTime();
-            NumberToWordsConverter.convertNumberToWords(number);
+            System.out.println(NumberToWordsConverter.convertNumberToWords(number));
             long endTime = System.nanoTime();
             total += (int) (endTime - startTime);
 
 
             long startTimePar = System.nanoTime();
-            ParallelNumberToWordsConverter.convertNumberToWordsParallel(number);
+            System.out.println(ParallelNumberToWordsConverter.convertNumberToWordsParallel(number));
             long endTimePar = System.nanoTime();
             totalPar += (int) (endTimePar - startTimePar);
         }
-        System.out.println("Average time for basic: "+total/1000);
-        System.out.println("Average time for parallel: "+totalPar/1000);
 
-    }
-
-    public static int randInt(int min, int max) {
-        Random rand = new Random();
-        return rand.nextInt((max - min) + 1) + min;
+        System.out.println("Average time for basic: " + total / 1000);
+        System.out.println("Average time for parallel: " + totalPar / 1000);
     }
 }
--- src/main/java/org/august/ParallelNumberToWordsConverter.java
+++ src/main/java/org/august/ParallelNumberToWordsConverter.java
@@ -52,6 +52,5 @@ public class ParallelNumberToWordsConverter {
             this.words.append(word);
         }
     }
-
 }
 
