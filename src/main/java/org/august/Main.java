58065f009f9370859546b9b48aa0b4bea3f255ab
Prashanna Sthapit <139867239+prashannasthapit@users.noreply.github.com>
2023-12-06 16:13:13 +0545

    simpler random number generator

--- src/main/java/org/august/Main.java
+++ src/main/java/org/august/Main.java
@@ -1,18 +1,15 @@
 package org.august;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int count = 10; // number of random integers generated
-        int minInclusive = -2147483648; // minimum (inclusive) value
-        int maxExclusive = 2147483647; // maximum (exclusive) value
-
-        // generate random numbers
-        SecureRandom secureRandom = new SecureRandom();
-        List<Integer> numbers = secureRandom.ints(count, minInclusive, maxExclusive).boxed().toList();
+        int count = 1000; // number of random integers generated
+        List<Integer> numbers = getIntegerList(count);
 
         // initialise variables for testing
         int total = 0;
@@ -59,5 +56,24 @@ public class Main {
         // output averages
         System.out.println("Average time for basic: " + (double) total / count + " nanoseconds");
         System.out.println("Average time for parallel: " + (double) totalPar / count + " nanoseconds");
+    }
+
+    private static List<Integer> getIntegerList(int count) {
+        List<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++) {
+            // Generate a random integer between Integer.MIN_VALUE and Integer.MAX_VALUE
+            numbers.add(random.nextInt());
+        }
+
+////        using secure random
+//        int minInclusive = -2147483648; // minimum (inclusive) value
+//        int maxExclusive = 2147483647; // maximum (exclusive) value
+////        generate random numbers
+//        SecureRandom secureRandom = new SecureRandom();
+//        numbers = secureRandom.ints(count, minInclusive, maxExclusive).boxed().toList();
+
+        return numbers;
     }
 }
